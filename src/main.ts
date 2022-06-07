d1844a18957058631c6119526745090a7aa4b813
Husseinat Etti-Balogun <hebalogun@gmail.com>
2022-06-07 14:30:59 +0100

    added swagger wrapper to view endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Demo NestJS Application')
+                      .setDescription("Demo NextJS API Application with JWT auth")
+                      .setVersion('v1')
+                      .addTag('products')
+                      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
